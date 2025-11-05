64bf84c4db7898fd399dae03ed06bcfee99e559a
GitHub Action <action@github.com>
2025-11-05 12:09:55 +0000

    Template cleanup

--- src/main/kotlin/com/github/layonez/feelexpressionlanguage/services/MyProjectService.kt
+++ src/main/kotlin/com/github/layonez/feelexpressionlanguage/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.layonez.feelexpressionlanguage.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.layonez.feelexpressionlanguage.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
